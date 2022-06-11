514d2ee86c9f7afc8b6abd8cfb6a3331374562c8
Emanuel-P16 <palazon.emanuel@gmail.com>
2022-06-11 09:57:30 -0300

    added heroCard component to show on screen every card of heroes

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -10,13 +10,13 @@ export const DashboardRoutes  = () => {
         <>
             <Navbar/>
             <div className='container'>
-            <Routes>
-                <Route path="marvel" element={<MarvelScreen/>}/>
-                <Route path="dc" element={<DcScreen/>}/>
-                <Route path="search" element={<SearchScreen/>} />
-                <Route path="hero" element={<HeroScreen/>} />
-                <Route path="/" element={<MarvelScreen/>}/>
-            </Routes>
+                <Routes>
+                    <Route path="marvel" element={<MarvelScreen/>}/>
+                    <Route path="dc" element={<DcScreen/>}/>
+                    <Route path="search" element={<SearchScreen/>} />
+                    <Route path="hero" element={<HeroScreen/>} />
+                    <Route path="/" element={<MarvelScreen/>}/>
+                </Routes>
             </div>
         </>
     )
